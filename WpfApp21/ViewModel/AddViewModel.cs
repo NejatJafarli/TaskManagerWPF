d8f1304daa12f43f2b171a61a26ff91da43b706e
nicat ceferli <mega.ceferli@gmail.com>
2021-12-21 22:29:48 +0400

    Add project files.

---
+++ WpfApp21/ViewModel/AddViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp21.Command;
+
+namespace WpfApp21.ViewModel
+{
+    public class AddViewModel : Window
+    {
+
+        public RelayCommand OkBtnCommand { get; set; }
+        public RelayCommand CancelBtnCommand { get; set; }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(AddViewModel));
+
+
+        public AddViewModel()
+        {
+
+            OkBtnCommand = new RelayCommand(s =>
+            {
+                if (s is Window w)
+                    if (!string.IsNullOrWhiteSpace(Text))
+                    {
+                        try
+                        {
+                            Process.Start(Text);
+                            MessageBox.Show($"{Text} Process Started");
+                            w.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"File Not Found ERROR Message: {ex.Message}");
+                        }
+                    }
+                    else
+                        MessageBox.Show("Text Is Empty");
+            });
+
+            CancelBtnCommand = new RelayCommand(s =>
+            {
+                if (s is Window w)
+                    w.Close();
+            });
+        }
+    }
+}
---
+++ WpfApp21/ViewModel/TaskManagerViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using WpfApp21.Command;
+using WpfApp21.Views;
+
+namespace WpfApp21.ViewModel
+{
+    public class TaskManagerViewModel : Window
+    {
+        public RelayCommand ExitAppCommand { get; set; }
+        public RelayCommand LoadedWindowCommand { get; set; }
+        public RelayCommand KillTaskCommand { get; set; }
+        public RelayCommand AddNewTaskCommand { get; set; }
+
+
+
+        public bool KillBtnEnabled
+        {
+            get { return (bool)GetValue(KillBtnEnabledProperty); }
+            set { SetValue(KillBtnEnabledProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for KillBtnEnabled.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty KillBtnEnabledProperty =
+            DependencyProperty.Register("KillBtnEnabled", typeof(bool), typeof(TaskManagerViewModel));
+
+
+
+        public int SelectedIndex
+        {
+            get { return (int)GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedItem.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register("SelectedIndex", typeof(int), typeof(TaskManagerViewModel));
+
+
+
+        public ObservableCollection<Process> Proces { get; set; } = new ObservableCollection<Process>(Process.GetProcesses().ToList().OrderBy(x => x.ProcessName).ToList());
+        public TaskManagerViewModel()
+        {
+            ExitAppCommand = new RelayCommand(s =>
+            {
+                  Application.Current.Shutdown();
+            });
+
+            AddNewTaskCommand = new RelayCommand(s =>
+            {
+                var temp = new AddView();
+                temp.ShowDialog();
+            });
+
+            LoadedWindowCommand = new RelayCommand(s =>
+            {
+                SelectedIndex = -1;
+                KillBtnEnabled = false;
+                DispatcherTimer timer = new DispatcherTimer();
+                timer.Interval = TimeSpan.FromSeconds(1);
+                timer.Tick += Timer_Tick;
+                timer.Start();
+            });
+
+            KillTaskCommand = new RelayCommand(s =>
+            {
+                if (SelectedIndex != -1)
+                    Proces[SelectedIndex].Kill();
+                else
+                    KillBtnEnabled=false;
+            });
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (SelectedIndex == -1)
+            {
+                KillBtnEnabled = false;
+                Proces.Clear();
+                var temp2 = Process.GetProcesses().ToList().OrderBy(x => x.ProcessName).ToList();
+                foreach (var item in temp2)
+                    Proces.Add(item);
+            }
+            else
+            {
+                KillBtnEnabled = true;
+                var temp = Proces[SelectedIndex].Id;
+                Proces.Clear();
+                var temp2 = Process.GetProcesses().ToList().OrderBy(x => x.ProcessName).ToList();
+                foreach (var process in temp2)
+                    Proces.Add(process);
+                SelectedIndex = Proces.ToList().FindIndex(x => x.Id == temp);
+            }
+        }
+    }
+}
